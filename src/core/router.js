175338fc67db58b82668c573384e92e746b551c8
jmarsal <jmarsal@student.42.fr>
2017-05-04 17:09:14 +0200

    debut de modification

--- src/core/router.js
+++ src/core/router.js
@@ -2,4 +2,31 @@
  * Created by jbmar on 01/05/2017.
  */
 
+import express from 'express';
+
+// Core
+import FrontController from './controllers/404_Controller';
+import AppController from './controllers/AccountController';
+import AuthController from './controllers/AccueilController';
+import AuthController from './controllers/AppController';
+import AuthController from './controllers/ProfilUserController';
+import AuthController from './controllers/RechercheController';
+
+class Router {
+    constructor() {
+        constructor() {
+            if (!app) {
+                console.error('Error! Exiting... You must provide the Express instance to the Router.');
+                process.exit(1);
+            }
+
+            this.registerRoutes();
+        }
+    }
+
+    registerRoutes() {
+
+    }
+}
+
 
