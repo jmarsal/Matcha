a87b1ea51ffd18a3369ee57b733ea18668ad231f
Marsal Jean-Baptiste <jmarsal@student.42.fr>
2017-05-17 10:36:05 +0200

    Du mieux... ca marche, mais sans ajax

--- public/js/ajaxAccount.js
+++ public/js/ajaxAccount.js
@@ -2,44 +2,69 @@
  * Created by jbmar on 16/05/2017.
  */
 
-function getUploadPhotoUser() {
-    let type = $("#my-file")[0].files[0].type,
-        fullNameExtension = $("#my-file")[0].files[0].name,
-        path = $("#my-file")[0].value,
-        size = $("#my-file")[0].files[0].size,
-        validImage = ["image/gif", "image/jpeg", "image/png"],
-        divError = $("#error")
-    ;
+$(document).ready(function () {
 
-    divError.removeClass('red green');
-    if (fullNameExtension) {
-        const reader = new FileReader();
+    // bind to the form's submit event
+    $('#frmUploader').submit(function () {
+        this.preventDefault();
 
-        if (validImage.indexOf(type) < 0) {
-            divError.addClass('red');
-            divError.text("Mauvais Format ! Veuillez essayer avec un jpg, png ou gif...");
-        } else {
-            if (size <= 1500000){
-                reader.addEventListener('load', () => {
-                    alert(reader.result);
-                    const tmp = "src=" + reader.result + "&namePhoto=" + fullNameExtension + "&typePhoto=" + type;
-                    $.post('/account/post', tmp, function (data, textStatus, jqXHR) {
-                        //var dataRes = JSON.parse(jqXHR.responseText),
-                            let divError = $("#error")
-                        ;
+        $(this).ajaxSubmit({
+            beforeSubmit: showRequest,  // pre-submit callback
+            success: showResponse  // post-submit callback
+        });
+        // always return false to prevent standard browser submit and page navigation
+        return false;
+    });
+});
 
-                        //if ((dataRes.isErr === 0 || dataRes.isErr === 1) && (dataRes.response)) {
-                        //    divError.removeClass('red green');
-                        //    dataRes.isErr === 1 ? divError.addClass('red') : divError.addClass('green');
-                        //}
-                            //divError.text(dataRes.response);
-                    });
-                });
-                reader.readAsDataURL($("#my-file")[0].files[0]);
-            } else {
-                divError.addClass('red');
-                divError.text("La taille de ta photo ne doit pas dépassée 1,5 mega !");
-            }
-        }
-    }
+// pre-submit callback
+function showRequest(formData, jqForm, options) {
+    alert('Uploading is starting.');
+    return true;
 }
+
+// post-submit callback
+function showResponse(responseText, statusText, xhr, $form) {
+    alert('status: ' + statusText + '\n\nresponseText: \n' + responseText );
+}
+// function getUploadPhotoUser() {
+//     let type = $("#my-file")[0].files[0].type,
+//         fullNameExtension = $("#my-file")[0].files[0].name,
+//         path = $("#my-file")[0].value,
+//         size = $("#my-file")[0].files[0].size,
+//         validImage = ["image/gif", "image/jpeg", "image/png"],
+//         divError = $("#error")
+//     ;
+//
+//     divError.removeClass('red green');
+//     if (fullNameExtension) {
+//         const reader = new FileReader();
+//
+//         if (validImage.indexOf(type) < 0) {
+//             divError.addClass('red');
+//             divError.text("Mauvais Format ! Veuillez essayer avec un jpg, png ou gif...");
+//         } else {
+//             if (size <= 1500000){
+//                 reader.addEventListener('load', () => {
+//                     alert(reader.result);
+//                     const tmp = "src=" + reader.result + "&namePhoto=" + fullNameExtension + "&typePhoto=" + type;
+//                     $.post('/account/post', tmp, function (data, textStatus, jqXHR) {
+//                         //var dataRes = JSON.parse(jqXHR.responseText),
+//                             let divError = $("#error")
+//                         ;
+//
+//                         //if ((dataRes.isErr === 0 || dataRes.isErr === 1) && (dataRes.response)) {
+//                         //    divError.removeClass('red green');
+//                         //    dataRes.isErr === 1 ? divError.addClass('red') : divError.addClass('green');
+//                         //}
+//                             //divError.text(dataRes.response);
+//                     });
+//                 });
+//                 reader.readAsDataURL($("#my-file")[0].files[0]);
+//             } else {
+//                 divError.addClass('red');
+//                 divError.text("La taille de ta photo ne doit pas dépassée 1,5 mega !");
+//             }
+//         }
+//     }
+// }
--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -6,6 +6,16 @@ const express = require('express');
 const Helper = require('../core/Helpers');
 const fs = require('fs');
 const uniqid = require('uniqid');
+const multer = require('multer');
+const Storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "./Images");
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
+const upload = multer({ storage: Storage }).array("imgUploader", 3); //Field name and max count
 
 class AccountController {
     constructor() {
@@ -38,12 +48,25 @@ class AccountController {
     }
 
     accountPostRoute() {
-        this.router.post('/account/post', (req, res) => {
-            if (req.session.start) {
+        this.router.post("/account/Upload", function (req, res) {
+            console.log('server');
+            upload(req, res, function (err) {
+                console.log('upload');
+                if (err) {
+                    console.error(err);
+                    return res.end("Something went wrong!");
+                }
+                res.render('views/account/accountContent', {
+                    error: "Image Upload !"
+                });
 
-                let namePhoto = req.body.namePhoto.replace('.jpg', '').replace('.jpeg', '').replace('.png', '').replace('.gif', ''),
-                    type = req.body.typePhoto.replace('image/', '')
-                ;
+                // Helper.sendResponseToClient("File uploaded sucessfully!.", 0, res);
+                // return res.end("File uploaded sucessfully!.");
+            });
+        });
+
+        // this.router.post('/account/post', (req, res) => {
+
                 //var data = req.body.src.replace(/^data:image\/\w+;base64,/, '');
                 //const buffer = new Buffer(req.body.src,'base64');
                 //console.log(buffer);
@@ -60,10 +83,10 @@ class AccountController {
                 //    console.log(err);
                 //});
                 //fs.writeFile("arghhhh.jpg", new Buffer(request.body.photo, "base64").toString(), function(err) {});
-            } else {
-                res.redirect('../accueil');
-            }
-        });
+            // } else {
+            //     res.redirect('../accueil');
+            // }
+        // });
     }
 }
 module.exports = AccountController;
