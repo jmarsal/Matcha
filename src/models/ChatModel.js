aa615dbbb8d080d69ced8c41b6159460b8cc6678
jmarsal <jmarsal@student.42.fr>
2017-06-21 17:05:54 +0200

    nouveau bug et reflexion sur la db pour les messages de chat et find quels users se like

--- src/models/ChatModel.js
+++ src/models/ChatModel.js
@@ -1,6 +1,15 @@
 class ChatModel {
 	static getUsersForChat(idUser) {
 		return new Promise((resolve, reject) => {
+			let sql =
+				'SELECT DISTINCT id_user_visit, login_user_visit, photo_user_visit FROM users_notifications WHERE likeUnlike = 1 && id_user = ?';
+
+			connection.query(sql, [ idUser ], (err, res) => {
+				if (err) {
+					reject(err);
+				}
+				resolve(res);
+			});
 			// recupere id, login, photo profil dans la table users, usersPhotoProfil
 			// recupere historique des messages
 		});
